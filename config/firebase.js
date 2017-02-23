696bac68e74dd9d41bf2e5cdbc01ee3e84b07649
dmfarcas <dan.farcas@assist.ro>
2017-02-23 15:29:12 +0200

    initial

---
+++ config/firebase.js
@@ -0,0 +1,7 @@
+export default {
+  apiKey: "AIzaSyBae3DaTkgc3CvAnuRr1Mlr60SktTxaqog",
+  authDomain: "book-database-4fba9.firebaseapp.com",
+  databaseURL: "https://book-database-4fba9.firebaseio.com",
+  storageBucket: "book-database-4fba9.appspot.com",
+  messagingSenderId: "929443717219"
+};
