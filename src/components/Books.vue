39df69a35acb43236333b78368d181ea3252c9ec
Dan Ich <danichimc@gmail.com>
2017-02-28 11:25:47 +0200

    #STR-7: add book component

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="book-container">
     <el-row v-loading.body="loading">
-      <el-col :span="8" v-for="book in books">
-        <el-card :body-style="{ padding: '0px' }">
-          <img :src="book.image.thumbnail" class="image">
-          <div style="padding: 14px;">
-            <span>{{ book.title }}</span>
-            <div class="bottom clearfix">
-              <small class="time">{{ book.description }}</small>
-              <el-button plain type="text" class="button" @click="details(book)">Details</el-button>
+
+      <div class="product-list-container clearfix">
+        <article class="product-list-item clearfix" v-for="book in books">
+          <a href="#" class="product-list-item-image-container cursor-hover">
+            <img class="product-list-item-image" :src="book.image.thumbnail" alt="{{ book.title }}">
+          </a>
+          <div class="product-list-item-price">
+            <div class="price-container">
+              <div class="price-old-container clearfix">
+                <span class="price-percent">Pagini:</span>
+              </div>
+              <span class="price-good">222</span>
             </div>
           </div>
-        </el-card>
-      </el-col>
+          <div class="product-list-item-info">
+            <h1 class="product-list-item-title">
+              <a class="cursor-hover">{{ book.title }}</a>
+            </h1>
+            <span class="product-list-item-author">{{ book.author }} </span>
+            <span class="product-list-item-brand"><span class="product-list-item-brand-type">Editura:</span> Nume</span>
+            <ul class="product-list-item-details">
+              <li><strong>Format</strong> : Paperback</li>
+              <li><strong>Data Publicarii</strong> : 06 Aug 2015</li>
+              <li><strong>ISBN</strong> : 9780241217931</li>
+            </ul>
+            <div class="product-list-item-share">
+              <span class="product-list-item-info-text">Share: </span>
+              <div class="social-icons">
+                <el-button plain type="text" class="button" @click="details(book['.key'])">Details</el-button>
+                <el-button plain type="text" class="button" @click="removeBook(book['.key'])">Delete</el-button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+
     </el-row>
     <book-details></book-details>
 
@@ -55,6 +79,7 @@
     mounted() {
       this.loading = true;
       booksRef.on('value', () => { //when data arrived
+        console.log('date ', booksRef);
         this.loading = false;
       })
     }
